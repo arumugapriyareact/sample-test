85c28cd3fbe20697f9201f7cdedb07d7181a5be4
Arumuga priya Iyyappan <arumugapriyaiyyappan@Arumugas-MacBook-Air.local>
2024-01-07 16:07:29 +0530

    done

--- functions/api.js
+++ functions/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const router = express.Router();
+const Razorpay = require("razorpay");
+const crypto = require("crypto");
 
 let records = [];
 
@@ -9,42 +11,72 @@ let records = [];
 router.get('/', (req, res) => {
   res.send('App is running..');
 });
+router.get("/check",async(req,res)=>{
+  try{
+      console.log("yas")
+  }catch(error){
+      console.log(error)
+  }
+  
+})
+router.post("/orders",async(req,res)=>{
+  console.log(req)
+  try{
+    
+      const instance= new Razorpay({
+          key_id: "rzp_test_GX4az0Kga9TjoI",
+          key_secret: "XwXATpfF8xcny6gYqCZWS4dR"
+      })
 
-//Create new record
-router.post('/add', (req, res) => {
-  res.send('New record added.');
-});
+      const options = {
+          amount: req.body.amount * 100,
+          currency: "INR",
+          receipt: crypto.randomBytes(10).toString("hex")
+      }
+      
+      instance.orders.create(options, (error, order)=>{
+          if(error){
+              console.log(error);
+              return res.status(500).json({message: "Something Went Wrong!"});
+          }
+          res.status(200).json({data:order});
+      })
 
-//delete existing record
-router.delete('/', (req, res) => {
-  res.send('Deleted existing record');
-});
-
-//updating existing record
-router.put('/', (req, res) => {
-  res.send('Updating existing record');
-});
-
-//showing demo records
-router.get('/demo', (req, res) => {
-  res.json([
-    {
-      id: '001',
-      name: 'Smith',
-      email: 'smith@gmail.com',
-    },
-    {
-      id: '002',
-      name: 'Sam',
-      email: 'sam@gmail.com',
-    },
-    {
-      id: '003',
-      name: 'lily',
-      email: 'lily@gmail.com',
-    },
-  ]);
-});
+  }catch(error){
+      console.log(error)
+      res.status(500).json({message: "Internal Server Error"})
+  }
+})
+router.get("/verify_get",async(req,res)=>{
+  try{
+      console.log("")
+  }catch(error){
+      console.log(error)
+      res.status(500).json({message: "Internal Server Error!"})
+  }
+})
+router.post("/verify",async(req,res)=>{
+  try{
+      const {
+          razorpay_order_id,
+          razorpay_payment_id,
+          razorpay_signature
+      } = req.body
+      const sign = `${razorpay_order_id}|${razorpay_payment_id}`
+      const expectedSign = crypto
+          .createHmac("sha256", "XwXATpfF8xcny6gYqCZWS4dR")
+          .update(sign.toString())
+          .digest("hex");
+          if(razorpay_signature === expectedSign){
+              return res.status(200).json({message: "Payment Verified Successfully"})
+          }else{
+              return res.status(400).json({message: "Invalid signature"})
+          }
+  }catch(error){
+      console.log(error)
+      res.status(500).json({message: "Internal Server Error!"})
+  }
+}) 
 
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
