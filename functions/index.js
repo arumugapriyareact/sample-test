dd8df797ae9a5ae3687b03c097b4d499c67713ac
Arumuga priya Iyyappan <arumugapriyaiyyappan@Arumugas-MacBook-Air.local>
2024-01-07 12:34:53 +0530

    testing

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const serverless = require("serverless-http")
+const cors = require("cors");
+const dotenv = require("dotenv");
+const paymentRoutes = require("./payment");
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/.netlify/functions/api",paymentRoutes)
+
+const port = process.env.PORT || 8000;
+app.listen(port,()=>{console.log(`Listening to port ${port}`)});
+module.exports.handler=serverless(app)
